436db74145af9fa3b5577a6f4d6a35c2b5900121
Urbana <ryan.kelley@ttu.edu>
2018-09-06 18:13:24 -0500

    Update camelcase.py

--- Algorithms/Strings/camelcase.py
+++ Algorithms/Strings/camelcase.py
@@ -4,10 +4,7 @@ import sys
 
 def camelcase(s):
     if len(s) == 0: return 0
-    total = 1
-    for x in s:
-        if x.isupper(): total += 1
-    return total
+    return len([x for x in s if x.isupper()]) + 1
                 
 
 if __name__ == "__main__":
