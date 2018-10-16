b93b0909845aa332f7c4a1df3d60456925eea38c
Urbana <ryan.kelley@ttu.edu>
2018-10-16 18:11:59 -0500

    Update caesar_cipher.py

--- Algorithms/Strings/caesar_cipher.py
+++ Algorithms/Strings/caesar_cipher.py
@@ -1,15 +1,16 @@
 #!/bin/python3
 
 import sys
+import string
 
 def caesarCipher(s, k):
-    alphabet = "abcdefghijklmnopqrstuvwxyz"
+    alpha = dict(zip(string.ascii_lowercase, range(len(string.ascii_lowercase))))
     encrypted = ""
-    for c in range(0, len(s)):
+    for c in range(len(s)):
         lo = s[c].lower()
-        if lo in alphabet:
-            index = (k + alphabet.find(lo)) % 26
-            char = alphabet[index]
+        if lo in alpha:
+            index = (k + alpha[lo]) % 26
+            char = string.ascii_lowercase[index]
             encrypted += (char if s[c].islower() else char.upper())
         else: encrypted += s[c]
     return encrypted
